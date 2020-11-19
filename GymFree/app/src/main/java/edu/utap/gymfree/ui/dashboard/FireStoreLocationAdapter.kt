abd9c2149758751c777b84a56a48c1fdc423149f
Sudarshan Bangalore <bsudarshan@outlook.com>
2020-11-19 18:56:18 -0500

    add del button, fix times

--- GymFree/app/src/main/java/edu/utap/gymfree/ui/dashboard/FireStoreLocationAdapter.kt
+++ GymFree/app/src/main/java/edu/utap/gymfree/ui/dashboard/FireStoreLocationAdapter.kt
@@ -1,6 +1,7 @@
 package edu.utap.gymfree.ui.dashboard
 
 import android.icu.text.SimpleDateFormat
+import android.opengl.Visibility
 import android.text.Html
 import android.util.Log
 import android.view.LayoutInflater
@@ -12,9 +13,11 @@ import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import edu.utap.gymfree.Location
 import edu.utap.gymfree.R
+import kotlinx.coroutines.delay
 
 class FireStoreLocationAdapter(private var viewModel: DashboardViewModel)
     : ListAdapter<Location, FireStoreLocationAdapter.VH>(Diff()) {
+    private val TAG = "adapter"
     // This class allows the adapter to compute what has changed
     class Diff : DiffUtil.ItemCallback<Location>() {
         override fun areItemsTheSame(oldItem: Location, newItem: Location): Boolean {
@@ -31,7 +34,7 @@ class FireStoreLocationAdapter(private var viewModel: DashboardViewModel)
         }
     }
     companion object {
-        private val dateFormat = SimpleDateFormat("hh:mm:ss MM-dd-yyyy")
+        private val dateFormat = SimpleDateFormat("hh:mm dd MMM, yyyy")
     }
 
     // ViewHolder pattern minimizes calls to findViewById
@@ -43,6 +46,8 @@ class FireStoreLocationAdapter(private var viewModel: DashboardViewModel)
         private var capacityTV = itemView.findViewById<TextView>(R.id.locationCapacityTV)
         private var openTV = itemView.findViewById<TextView>(R.id.locationOpeningTV)
         private var closeTV = itemView.findViewById<TextView>(R.id.locationClosingTV)
+        private var delBut = itemView.findViewById<com.google.android.material.button.MaterialButton>(R.id.deleteButton)
+        private var delButSure = itemView.findViewById<com.google.android.material.button.MaterialButton>(R.id.deleteButtonSure)
 
         private fun bindElements(item: Location,
                                  userTV: TextView,
@@ -50,15 +55,63 @@ class FireStoreLocationAdapter(private var viewModel: DashboardViewModel)
                                  addressTV: TextView,
                                  capacityTV: TextView,
                                  openTV: TextView,
-                                 closeTV: TextView
+                                 closeTV: TextView,
+                                 delBut: com.google.android.material.button.MaterialButton,
+                                 delButSure: com.google.android.material.button.MaterialButton
         ) {
             Log.i("XXX-ADAPTER-bindels", item.toString())
             userTV.text = Html.fromHtml(item.name)
             addressTV.text = Html.fromHtml("<b>Address</b>: " + item.address.toString())
             capacityTV.text = Html.fromHtml("<b>Capacity</b>: " + item.capacity?.toInt().toString())
-            openTV.text = Html.fromHtml("<b>Opening time</b>: " + item.openingTime.toString())
-            closeTV.text = Html.fromHtml("<b>Closing time</b>: " +  item.closingTime.toString())
+            var openingHour = item.openingTime?.toInt().toString()
+            var openingMins = item.openingTime?.minus(item.openingTime?.toInt()!!)?.times(60)?.toInt().toString()
+            var closingHour = item.closingTime?.toInt().toString()
+            var closingMins = item.closingTime?.minus(item.closingTime?.toInt()!!)?.times(60)?.toInt().toString()
+            if (item.openingTime?.toInt()!! < 10) {
+                openingHour = "0$openingHour"
+            }
+            if (item.openingTime?.minus(item.openingTime?.toInt()!!)?.times(60)?.toInt()!! < 10) {
+                openingMins = "0$openingMins"
+            }
+            if (item.closingTime?.toInt()!! < 10) {
+                closingHour = "0$closingHour"
+            }
+            if (item.closingTime?.minus(item.closingTime?.toInt()!!)?.times(60)?.toInt()!! < 10) {
+                closingMins = "0$closingMins"
+            }
+            openTV.text = Html.fromHtml("<b>Opening</b>: $openingHour:$openingMins")
+            closeTV.text = Html.fromHtml("<b>Closing</b>: $closingHour:$closingMins")
             timeTV.text = Html.fromHtml("<b>Created on</b>: " + dateFormat.format(item.timeStamp?.toDate()).toString())
+
+            delBut.setOnClickListener {
+                Log.i(TAG,"longclick - delbut")
+                delBut.visibility = View.GONE
+                delButSure.visibility = View.VISIBLE
+                Thread.sleep(2000)
+                delBut.visibility = View.VISIBLE
+                delButSure.visibility = View.GONE
+            }
+
+            delBut.setOnLongClickListener {
+                Log.i(TAG,"longclick - delbutsure")
+                delBut.visibility = View.GONE
+                delButSure.visibility = View.VISIBLE
+                Thread.sleep(2000)
+                delBut.visibility = View.VISIBLE
+                delButSure.visibility = View.GONE
+                true
+            }
+
+            delButSure.setOnClickListener {
+                Log.i(TAG,"clik - delbut")
+                viewModel.deleteLocation(item)
+            }
+
+            delButSure.setOnLongClickListener {
+                Log.i(TAG,"clik - delbutsure")
+                viewModel.deleteLocation(item)
+                true
+            }
 
             userTV.visibility = View.VISIBLE
             addressTV.visibility = View.VISIBLE
@@ -66,6 +119,7 @@ class FireStoreLocationAdapter(private var viewModel: DashboardViewModel)
             openTV.visibility = View.VISIBLE
             closeTV.visibility = View.VISIBLE
             timeTV.visibility = View.VISIBLE
+            delButSure.visibility = View.GONE
         }
 
         fun bind(item: Location?) {
@@ -80,7 +134,9 @@ class FireStoreLocationAdapter(private var viewModel: DashboardViewModel)
                     addressTV,
                     capacityTV,
                     openTV,
-                    closeTV
+                    closeTV,
+                    delBut,
+                    delButSure
             )
         }
     }
