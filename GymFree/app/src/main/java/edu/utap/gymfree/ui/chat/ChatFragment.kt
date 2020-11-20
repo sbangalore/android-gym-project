92ac984538a47be514fe33fe91349ada9a2420f9
NirmalMulji <nirmalmulji@gmail.com>
2020-11-19 21:38:21 -0600

    added member chat, issue with layout

--- GymFree/app/src/main/java/edu/utap/gymfree/ui/chat/ChatFragment.kt
+++ GymFree/app/src/main/java/edu/utap/gymfree/ui/chat/ChatFragment.kt
@@ -10,22 +10,137 @@ import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import edu.utap.gymfree.R
 
+import android.util.Log
+import android.view.KeyEvent
+import android.view.inputmethod.EditorInfo
+import androidx.core.view.doOnLayout
+import androidx.core.view.updateLayoutParams
+import androidx.fragment.app.activityViewModels
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.auth.FirebaseUser
+import kotlinx.android.synthetic.main.fragment_chat.*
+
 class ChatFragment : Fragment() {
 
     private lateinit var chatViewModel: ChatViewModel
+    private lateinit var chatAdapter: FirestoreChatAdapter
+    private var currentUser: FirebaseUser? = null
+    private var fragmentUUID: String? = null
 
     override fun onCreateView(
-            inflater: LayoutInflater,
-            container: ViewGroup?,
-            savedInstanceState: Bundle?
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View? {
         chatViewModel =
-                ViewModelProvider(this).get(ChatViewModel::class.java)
+            ViewModelProvider(this).get(ChatViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_chat, container, false)
-        val textView: TextView = root.findViewById(R.id.text_chat)
-        chatViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
+//        val textView: TextView = root.findViewById(R.id.text_chat)
+//        chatViewModel.text.observe(viewLifecycleOwner, Observer {
+//            textView.text = it
+//        })
         return root
+    }
+
+
+
+    private fun scrollToEnd() =
+        (chatAdapter.itemCount - 1).takeIf { it > 0 }?.let(chatRV::smoothScrollToPosition)
+    private fun initRecyclerView()  {
+        chatAdapter = FirestoreChatAdapter(chatViewModel)
+        chatRV.adapter = chatAdapter
+        chatRV.layoutManager = LinearLayoutManager(context)
+        //https://stackoverflow.com/questions/26580723/how-to-scroll-to-the-bottom-of-a-recyclerview-scrolltoposition-doesnt-work
+        chatRV.viewTreeObserver.addOnGlobalLayoutListener {
+            scrollToEnd()
+        }
+        // Dividers not so nice in chat
+    }
+    private fun clearCompose() {
+        // XXX Write me
+        composeMessageET.text.clear()
+//        composePreviewIV.setImageResource(android.R.color.transparent)
+
+
+    }
+    private fun initAuth() {
+        chatViewModel.observeFirebaseAuthLiveData().observe(viewLifecycleOwner, Observer {
+            currentUser = it
+        })
+    }
+    // For our phone, translate dp to pixels
+    private fun dpToPx(dp: Int): Int {
+        return (dp * resources.displayMetrics.density).toInt()
+    }
+
+    private fun initComposeSendIB() {
+        // Send message button
+        composeSendIB.setOnClickListener {
+            if( composeMessageET.text.isNotEmpty()) {
+                val chatRow = ChatRow().apply {
+                    val cUser = currentUser
+                    if(cUser == null) {
+                        name = "unknown"
+                        ownerUid = "unknown"
+                        Log.d("ChatFragment", "XXX, currentUser null!")
+                    } else {
+                        name = cUser.displayName
+                        ownerUid = cUser.uid
+                    }
+                    message = composeMessageET.text.toString()
+                    pictureUUID = fragmentUUID
+                    clearCompose()
+                }
+                chatViewModel.saveChatRow(chatRow)
+            }
+        }
+    }
+
+    // Something might have changed.  Redo query
+    override fun onResume() {
+        super.onResume()
+        chatViewModel.getChat()
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        initAuth()
+        initComposeSendIB()
+        initRecyclerView()
+
+        chatViewModel.observeChat().observe(viewLifecycleOwner, Observer {
+            Log.d(javaClass.simpleName, "Observe Chat $it")
+            chatAdapter.submitList(it)
+        })
+
+        composeMessageET.setOnEditorActionListener { /*v*/_, actionId, event ->
+            // If user has pressed enter, or if they hit the soft keyboard "send" button
+            // (which sends DONE because of the XML)
+            if ((event != null
+                        &&(event.action == KeyEvent.ACTION_DOWN)
+                        &&(event.keyCode == KeyEvent.KEYCODE_ENTER))
+                || (actionId == EditorInfo.IME_ACTION_DONE)) {
+//                (requireActivity() as MainActivity).hideKeyboard()
+                composeSendIB.callOnClick()
+            }
+            true
+        }
+//        composeCameraIB.setOnClickListener {
+//            chatViewModel.takePhoto {
+//                Log.d(javaClass.simpleName, "uuid $it")
+//                fragmentUUID = it
+//                composePreviewIV.doOnLayout {view ->
+//                    view.updateLayoutParams {
+//                        width = chatViewModel.fourFifthWidthPx
+//                    }
+//                }
+//                composePreviewIV.visibility = View.VISIBLE
+//                chatViewModel.glideFetch(it, composePreviewIV)
+//            }
+//        }
+//        composePreviewIV.visibility = View.GONE
+        //Log.d(javaClass.simpleName, "vm 1/5 ${viewModel.oneFifthWidthPx} mine ${(resources.displayMetrics.widthPixels / 5).toInt()}")
     }
 }
--- GymFree/app/src/main/java/edu/utap/gymfree/ui/chat/ChatViewModel.kt
+++ GymFree/app/src/main/java/edu/utap/gymfree/ui/chat/ChatViewModel.kt
@@ -1,13 +1,240 @@
 package edu.utap.gymfree.ui.chat
 
+import android.app.Application
+import android.net.Uri
+import android.os.Environment
+import android.util.Log
+import android.widget.ImageView
+import androidx.core.content.FileProvider
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.ListenerRegistration
+import edu.utap.firechat.FirestoreAuthLiveData
+import edu.utap.gymfree.Location
+import java.io.File
+import java.io.IOException
+import java.util.*
+
 
 class ChatViewModel : ViewModel() {
+    private val TAG = "XXX-ChatViewModel"
+    private val OWNER_EMAIL = "owner@example.com"
 
     private val _text = MutableLiveData<String>().apply {
         value = "This is chat Fragment"
     }
     val text: LiveData<String> = _text
+
+
+//    private val appContext = getApplication<Application>().applicationContext
+//    private var storageDir =
+//        getApplication<Application>().getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+    private var db: FirebaseFirestore = FirebaseFirestore.getInstance()
+    private var firebaseAuthLiveData = FirestoreAuthLiveData()
+    private var chat = MutableLiveData<List<ChatRow>>()
+    private var chatListener : ListenerRegistration? = null
+//    private val oneFifthWidthPx = (appContext.resources.displayMetrics.widthPixels / 5).toInt()
+//    val fourFifthWidthPx = 4 * oneFifthWidthPx
+    // assert does not work
+    private lateinit var crashMe: String
+
+    // Default function implementations for photoSuccess callback
+    private fun noPhoto() {
+        Log.d(javaClass.simpleName, "Function must be initialized to something that can start the camera intent")
+        crashMe.plus(" ")
+    }
+    private var takePhotoIntent: () -> Unit = ::noPhoto
+
+    private fun defaultPhoto(@Suppress("UNUSED_PARAMETER") path: String) {
+        Log.d(javaClass.simpleName, "Function must be initialized to photo callback" )
+        crashMe.plus(" ")
+    }
+    private var photoSuccess: (path: String) -> Unit = ::defaultPhoto
+
+
+    fun observeFirebaseAuthLiveData(): LiveData<FirebaseUser?> {
+        return firebaseAuthLiveData
+    }
+    fun myUid(): String? {
+        return firebaseAuthLiveData.value?.uid
+    }
+    fun signOut() {
+        chatListener?.remove()
+        FirebaseAuth.getInstance().signOut()
+        chat.value = listOf()
+    }
+
+    fun observeChat(): LiveData<List<ChatRow>> {
+        return chat
+    }
+    fun saveChatRow(chatRow: ChatRow) {
+        Log.d(
+            "ChatViewModel",
+            String.format(
+                "saveChatRow ownerUid(%s) name(%s) %s",
+                chatRow.ownerUid,
+                chatRow.name,
+                chatRow.message
+            )
+        )
+
+        // XXX Write me.
+        // https://firebase.google.com/docs/firestore/manage-data/add-data#add_a_document
+        // Remember to set the rowID of the chatRow before saving it
+        chatRow.rowID = db.collection("memberChat").document().id
+
+        db.collection("memberChat")
+            .document(chatRow.rowID)
+            .set(chatRow)
+            .addOnSuccessListener {
+                Log.d(javaClass.simpleName, "Chatrow written with ID: ${chatRow.rowID}")
+            }
+            .addOnFailureListener{
+                Log.w(javaClass.simpleName, "Error adding chatrow", it)
+            }
+    }
+
+    fun deleteChatRow(chatRow: ChatRow){
+        // Delete picture (if any) on the server, asynchronously
+//        val uuid = chatRow.pictureUUID
+//        if(uuid != null) {
+//            Storage.deleteImage(uuid)
+//        }
+        Log.d(javaClass.simpleName, "remote chatRow id: ${chatRow.rowID}")
+
+        // XXX delete chatRow
+        db.collection("memberChat").document(chatRow.rowID).delete()
+            .addOnSuccessListener {
+                Log.d(javaClass.simpleName, "Delete Row Success: ${chatRow.rowID}")
+            }
+            .addOnFailureListener{
+                Log.d(javaClass.simpleName, "Delete Row Failed: ${chatRow.rowID}")
+                Log.w(javaClass.simpleName, "Error deleting row", it)
+
+            }
+
+
+
+    }
+
+    fun getChat() {
+        if(FirebaseAuth.getInstance().currentUser == null) {
+            Log.d(javaClass.simpleName, "Can't get chat, no one is logged in")
+            chat.value = listOf()
+            return
+        }
+        // XXX Write me.  Limit total number of chat rows to 100
+        db.collection("memberChat")
+            .orderBy("timeStamp")
+            .limit(100)
+            .addSnapshotListener { querySnapshot, ex ->
+                if (ex != null) {
+                    Log.w(TAG, "listen:error", ex)
+                    return@addSnapshotListener
+                }
+
+                Log.d(TAG, "fetch ${querySnapshot!!.documents.size}")
+                chat.value = querySnapshot.documents.mapNotNull {
+                    it.toObject(ChatRow::class.java)
+                }
+            }
+
+    }
+
+    /////////////////////////////////////////////////////////////
+    // This is intended to be set once by MainActivity.
+    // The bummer is that taking a photo requires startActivityForResult
+    // which has to be called from an activity.
+    fun setPhotoIntent(_takePhotoIntent: () -> Unit) {
+        takePhotoIntent = _takePhotoIntent
+        //state.set(takePhotoIntentKey, takePhotoIntent)
+    }
+
+    /////////////////////////////////////////////////////////////
+    // Get callback for when camera intent returns.
+    // Send intent to take picture
+    fun takePhoto(_photoSuccess: (String) -> Unit) {
+        photoSuccess = _photoSuccess
+        //state.set(photoSuccessKey, photoSuccess)
+        takePhotoIntent.invoke()
+    }
+
+    /////////////////////////////////////////////////////////////
+    // Create a file for the photo, remember it, and create a Uri
+//    fun getPhotoURI(): Uri {
+//        // Create an image file name
+//        state.set(pictureUUIDKey,  UUID.randomUUID().toString())
+//        Log.d(javaClass.simpleName, "pictureUUID ${state.get<String>(pictureUUIDKey)}")
+//        var photoUri: Uri? = null
+//        // Create the File where the photo should go
+//        try {
+//            val localPhotoFile = File(storageDir, "${state.get<String>(pictureUUIDKey)}.jpg")
+//            Log.d(javaClass.simpleName, "photo path ${localPhotoFile.absolutePath}")
+//            photoUri = FileProvider.getUriForFile(
+//                appContext,
+//                "edu.utap.firechat",
+//                localPhotoFile
+//            )
+//        } catch (ex: IOException) {
+//            // Error occurred while creating the File
+//            Log.d(javaClass.simpleName, "Cannot create file", ex)
+//        }
+//        // CRASH.  Production code should do something more graceful
+//        return photoUri!!
+//    }
+    /////////////////////////////////////////////////////////////
+    // Callbacks from MainActivity.getResultForActivity from camera intent
+    // We can't just schedule the file upload and return.
+    // The problem is that our previous picture uploads can still be pending.
+    // So a note can have a pictureUUID that does not refer to an existing file.
+    // That violates referential integrity, which we really like in our db (and programming
+    // model).
+    // So we do not add the pictureUUID to the note until the picture finishes uploading.
+    // That means a user won't see their picture updates immediately, they have to
+    // wait for some interaction with the server.
+    // You could imagine dealing with this somehow using local files while waiting for
+    // a server interaction, but that seems error prone.
+    // Freezing the app during an upload also seems bad.
+//    fun pictureSuccess() {
+//        val pictureUUID = state.get(pictureUUIDKey) ?: ""
+//        val localPhotoFile = File(storageDir, "${pictureUUID}.jpg")
+//        Log.d(javaClass.simpleName, "pictureSuccess ${localPhotoFile.absolutePath}")
+//        // Wait until photo is successfully uploaded before calling back
+//        Storage.uploadImage(localPhotoFile, pictureUUID) {
+//            Log.d(javaClass.simpleName, "uploadImage callback ${pictureUUID}")
+//            photoSuccess(pictureUUID)
+//            photoSuccess = ::defaultPhoto
+////            state.get<(String)->Unit>(photoSuccessKey)?.invoke(pictureUUID)
+////            state.set(photoSuccessKey, ::defaultPhoto)
+//            state.set(pictureUUIDKey, "")
+//        }
+//    }
+//    fun pictureFailure() {
+//        // Note, the camera intent will only create the file if the user hits accept
+//        // so I've never seen this called
+//        state.set(pictureUUIDKey, "")
+//        Log.d(javaClass.simpleName, "pictureFailure pictureUUID cleared")
+//    }
+
+    // For our phone, translate dp to pixels
+//    private fun dpToPx(dp: Int): Int {
+//        return (dp * appContext.resources.displayMetrics.density).toInt()
+//    }
+//    fun glideFetch(pictureUUID: String, imageView: ImageView) {
+//        // NB: Should get apsect ratio from image itself
+//        Glide.fetch(Storage.uuid2StorageReference(pictureUUID), imageView,
+//            fourFifthWidthPx, (1.466*fourFifthWidthPx).toInt())
+//    }
+    // Debatable how useful this is.
+    override fun onCleared() {
+        Log.d(javaClass.simpleName, "onCleared!!")
+        super.onCleared()
+        chatListener?.remove()
+    }
+
+
 }
